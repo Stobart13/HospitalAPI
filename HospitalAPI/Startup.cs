e822e1eb6aeb40013fef1710c3cb892901fe0946
Stuart Kinnear <stobart13@gmail.com>
2021-07-29 00:10:37 +0100

    Changing LaunchSettings.json to point to Swagger UI on launch

--- HospitalAPI/Startup.cs
+++ HospitalAPI/Startup.cs
@@ -38,7 +38,7 @@ namespace HospitalAPI
 
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "PinPoint.PIM.API", Version = "v1" });
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "HospitalAPI", Version = "v1" });
             });
 
             services.AddTransient<IHospitalRepository>(s => new HospitalRepository(_hospitalConnectionString));
@@ -51,7 +51,7 @@ namespace HospitalAPI
             {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "PinPoint.PIM.API v1"));
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "HospitalAPI"));
             }
 
             app.UseHttpsRedirection();
